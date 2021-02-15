9205d4231cd8de50523db4aab75a25307a5ee0f6
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-02-14 20:24:55 -0800

    changed to character controller

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
-    public Rigidbody2D rigidbody;
+    public CharacterController player;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +19,10 @@ public class playerMovement : MonoBehaviour
     //Update is called once per frame
     void FixedUpdate()
     {
-        float xInput = Input.GetAxis("Horizontal") * moveSpeed;
-        float yInput = Input.GetAxis("Vertical") * moveSpeed;
+        float xInput = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float yInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         Debug.Log($"{Input.GetAxis("Horizontal")}, {Input.GetAxis("Vertical")}");
         Vector2 force = new Vector2(xInput, yInput);
-        rigidbody.velocity = force;
+        player.Move(force);
     }
 }
