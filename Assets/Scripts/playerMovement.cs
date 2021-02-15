76d48ca6103e149913b2e11e0543fc870db686de
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-02-14 22:05:19 -0800

    working (input system sucks) gun aim

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,28 +1,43 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public CharacterController player;
+    public Transform gun;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
-        
-    
-    
     //Update is called once per frame
     void FixedUpdate()
     {
+        move();
+        look();
+    }
+
+    private void look()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        //TODO: INPUT
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        gun.transform.rotation = Quaternion.AngleAxis(angle+180, Vector3.forward);
+    }
+    
+    private void move()
+    {
+        //TODO: INPUT
         float xInput = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float yInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        Debug.Log($"{Input.GetAxis("Horizontal")}, {Input.GetAxis("Vertical")}");
         Vector2 force = new Vector2(xInput, yInput);
         player.Move(force);
     }
+    
+    
+    
 }
