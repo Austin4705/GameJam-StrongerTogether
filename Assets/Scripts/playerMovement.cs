4c9fcd1aa45c1271f6d9e725f1ff5a080c5e844b
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-02-14 20:15:59 -0800

    added physics player movement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+        
+    
+    
+    //Update is called once per frame
+    void FixedUpdate()
+    {
+        float xInput = Input.GetAxis("Horizontal") * moveSpeed;
+        float yInput = Input.GetAxis("Vertical") * moveSpeed;
+        Debug.Log($"{Input.GetAxis("Horizontal")}, {Input.GetAxis("Vertical")}");
+        Vector2 force = new Vector2(xInput, yInput);
+        rigidbody.velocity = force;
+    }
+}
