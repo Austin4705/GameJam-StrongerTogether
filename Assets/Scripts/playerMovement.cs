ae72e8720443c2d29791746439a72305488c8f6d
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-02-14 23:05:31 -0800

    working collision

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
-    public CharacterController player;
+    public Rigidbody2D player;
     public Transform gun;
     // Start is called before the first frame update
     void Start()
@@ -32,10 +32,12 @@ public class playerMovement : MonoBehaviour
     private void move()
     {
         //TODO: INPUT
-        float xInput = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float yInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float xInput = Input.GetAxis("Horizontal") * moveSpeed;
+        float yInput = Input.GetAxis("Vertical") * moveSpeed;
+        Debug.Log($"{Input.GetAxis("Horizontal")}, {Input.GetAxis("Vertical")}");
         Vector2 force = new Vector2(xInput, yInput);
-        player.Move(force);
+        player.velocity = force;
+        //player.Move(force);
     }
     
     
