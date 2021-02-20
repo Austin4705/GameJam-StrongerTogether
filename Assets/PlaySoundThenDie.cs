106f55b748102942e809fe926b19306a52677731
Dario Quintero <dario.r.quintero@gmail.com>
2021-02-19 19:33:36 -0800

    Sounds Made Louder

--- Assets/PlaySoundThenDie.cs
+++ Assets/PlaySoundThenDie.cs
@@ -15,10 +15,16 @@ public class PlaySoundThenDie : MonoBehaviour
 
     private IEnumerator Sound(AudioClip clip)
     {
-        AudioSource.PlayClipAtPoint(clip, transform.position);
+        PlaySound();
 
         yield return new WaitForSeconds(clip.length+0.1f); //extra second added for buffer
 
         Destroy(gameObject);
+    }
+
+    private void PlaySound()
+    {
+        source.clip = clip;
+        source.Play();
     }
 }
