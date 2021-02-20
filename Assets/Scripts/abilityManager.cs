fa428ebc57f79e1cbd70eda33329b4051bde8e43
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-02-19 20:27:14 -0800

    save on hopefully fully working ability system (tho most likely not)

--- Assets/Scripts/abilityManager.cs
+++ Assets/Scripts/abilityManager.cs
@@ -16,6 +16,26 @@ public class abilityManager : MonoBehaviour
     public GameObject gunTip;
     public GameObject orb;
     public GameObject C4;
+    
+    public float C4CoolDown;
+    public int C4Price;
+    public float C4Timer;
+    public event Action detonation;
+    
+    public float piercingCoolDown;
+    public int piercingPrice;
+    public float piercingTimer;
+    public float piercingActiveTime;
+
+    public float machineGunCoolDown;
+    public int machineGunPrice;
+    public float machineGunTimer;
+    public float machineGunActiveTime;
+
+    public float orbCoolDown;
+    public int orbPrice;
+    public float orbTimer;
+
     public static abilityManager Instance { get { return _instance; } }
     private void Awake()
     {
@@ -30,34 +50,51 @@ public class abilityManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        C4Timer = Time.time;
+        piercingTimer = Time.time;
+        machineGunTimer = Time.time;
+        orbTimer = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
+        if (Time.time - piercingTimer > piercingActiveTime)
+        {
+            piercingEnabled = false;
+        }
+        if (Time.time - machineGunTimer > machineGunActiveTime)
+        {
+            machineGunEnabled = false;
+        }
         //TODO: input
-        if (Input.GetKeyDown(KeyCode.G) && c4Unlocked)
+        if (Input.GetKeyDown(KeyCode.G) && c4Unlocked && Time.time - C4Timer > C4CoolDown && this.GetComponent<nanobotSystem>().nanobots - C4Price > 0)
         {
             spawnC4();
+            C4Timer = Time.time;
+            this.GetComponent<nanobotSystem>().nanobots -= C4Price;
         }
         if (Input.GetKeyDown(KeyCode.F) && c4Unlocked)
         {
             detonateC4();
         }
-        if (Input.GetKeyDown(KeyCode.H) && piercingUnlocked)
+        if (Input.GetKeyDown(KeyCode.H) && piercingUnlocked && Time.time - piercingTimer > piercingCoolDown && this.GetComponent<nanobotSystem>().nanobots - piercingPrice > 0)
         {
             enablePiecingBullets();
+            piercingTimer = Time.time;
+            this.GetComponent<nanobotSystem>().nanobots -= piercingPrice;
         }
-        if (Input.GetKeyDown(KeyCode.Y) && machineGunUnlocked)
+        if (Input.GetKeyDown(KeyCode.Y) && machineGunUnlocked && Time.time - machineGunTimer > machineGunCoolDown && this.GetComponent<nanobotSystem>().nanobots - machineGunPrice > 0)
         {
             enableMachineGun();
+            machineGunTimer = Time.time;
+            this.GetComponent<nanobotSystem>().nanobots -= machineGunPrice;
         }
-        if (Input.GetKeyDown(KeyCode.T) && orbUnlocked)
+        if (Input.GetKeyDown(KeyCode.T) && orbUnlocked && Time.time - orbTimer > orbCoolDown && this.GetComponent<nanobotSystem>().nanobots - orbPrice > 0)
         {
             spawnOrb();
+            orbTimer = Time.time;
+            this.GetComponent<nanobotSystem>().nanobots -= orbPrice;
         }
     }
 
@@ -75,7 +112,10 @@ public class abilityManager : MonoBehaviour
 
     private void detonateC4()
     {
-        
+        if(detonation != null)
+        {
+            detonation();
+        }
     }
     private void enablePiecingBullets()
     {
