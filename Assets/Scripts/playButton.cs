e27d4dfc5713d49b1f174b9e38f2cda4079d6d89
Dario Quintero <dario.r.quintero@gmail.com>
2021-02-20 23:39:02 -0800

    Menu Screen Updates round ui fixed

--- Assets/Scripts/playButton.cs
+++ Assets/Scripts/playButton.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class playButton : MonoBehaviour
 {
+    //public audio
     public void play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnMouseEnter()
+    {
+        
     }
 
     public void quit()
